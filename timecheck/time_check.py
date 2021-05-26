1c6008ce312334c03c76deed05445ecdb9c54fb2
Ali Barani <29902054+abarani30@users.noreply.github.com>
2021-05-26 09:36:05 +0300

    Adding A Try Except Approach

--- timecheck/time_check.py
+++ timecheck/time_check.py
@@ -1,17 +1,20 @@
 class TimeCheck:
     def validTime(self, time):
-        if len(time) != 5 or ":" not in time:
-            return False
-        hours = time.split(":")[0]
-        minutes = time.split(":")[1]
-        if len(hours) != 2 or len(minutes) != 2:
-            return False
-        else:
-            if int(hours) <= 23 and int(minutes) <= 59:
+        try:
+            if len(time) != 5 or ":" not in time:
+                return False
+            hours = time.split(":")[0]
+            minutes = time.split(":")[1]
+            if (len(hours) == 2 and len(minutes) == 2 and hours.isnumeric()
+                    and minutes.isnumeric() and int(hours) <= 23 and int(minutes) <= 59):
                 return True
-        return False
+            else:
+                return False
+
+        except ValueError:
+            return False
 
 
 if __name__ == "__main__":
     myObj = TimeCheck()
-    print(myObj.validTime("01:45"))
+    print(myObj.validTime("02:42"))
